a9d3866f02c3d7525d8374c4a83ba3cb746a84d5
YZ <13791477468@163.com>
2022-06-10 16:33:36 +0800

    完成了主页开发

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function () {
+    getuserInfo();
+    var layer = layui.layer;
+    $("#btnLogout").on('click',function (){
+        // console.log('ok');
+        // 提示用户是否退出
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            localStorage.removeItem('token');
+            location.href='/login.html'
+            layer.close(index);
+          });
+    })
+});
+// 获取用户的基本信息
+function getuserInfo() {
+  $.ajax({
+    method: "GET",
+    url: "/my/userinfo",
+    success: function (res) {
+      // console.log(res);
+      if (res.status !== 0) {
+        return layui.layer.msg("获取用户信息失败");
+      }
+      // 调用renderAvatar函数渲染用户头像
+      renderAvatar(res.data);
+      },
+  });
+}
+// 创建函数
+function renderAvatar(user) {
+  var name = user.nickname || user.username;
+  $("#weclome").html("欢迎&nbsp;&nbsp;" + name);
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+      $(".layui-nav-img").hide();
+      var first = name[0].toUpperCase();
+      $(".text-avatar").html(first).show();
+  }
+}
